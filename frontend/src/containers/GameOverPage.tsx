ebf10971e4f78d59bdd7d41c04295dba45df2f2b
loganCurd <logan.curd@outlook.com>
2023-04-12 14:24:06 -0500

    initial commit

---
+++ frontend/src/containers/GameOverPage.tsx
@@ -0,0 +1,11 @@
+
+
+export const GameOverPage = () => {
+    return (
+        <div>
+            <br />
+            <h1>GAME OVER</h1>
+            <h4>Thanks for playing!</h4>
+        </div>
+    )
+}
