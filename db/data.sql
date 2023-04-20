c273cf64b1962bb1abfbd6611946fed42591b444
David Perez <david.a.perez@vanderbilt.edu>
2023-04-20 16:34:24 -0500

    Add containerized workshops

--- db/data.sql
+++ db/data.sql
@@ -2,6 +2,7 @@ INSERT INTO repository(name, filename, url, description)
 VALUES ('GitOxide', '../GitGuessr-data/gitoxide', 'https://github.com/Byron/gitoxide', 'An idiomatic, lean, fast & safe pure Rust implementation of Git'),
     ('Tree-sitter', '../GitGuessr-data/tree-sitter', 'https://github.com/tree-sitter/tree-sitter', 'An incremental parsing system for programming tools'),
     ('GitGuessr', '.', 'https://github.com/david-a-perez/GitGuessr-website', 'Tool to get familiar with a codebase through fun games'),
+    ('Containerized Workshops', '../GitGuessr-data/containerized-workshops', 'https://github.com/david-a-perez/containerized-workshops', 'Tool for workshop delivery through cloud containers'),
     ('Linux', '../GitGuessr-data/linux', 'https://www.kernel.org/', 'The Linux kernel source code') ON CONFLICT (name) DO
 UPDATE
 SET filename = EXCLUDED.filename;
@@ -9,6 +10,7 @@ INSERT INTO obfuscated_game_format_config(repository_id, language, filenames)
 VALUES ('GitOxide', 'rust', '\.rs$'),
     ('Tree-sitter', 'rust', '\.rs$'),
     ('GitGuessr', 'rust', '\.rs$'),
+    ('Containerized Workshops', 'python', 'ContainerizedWorkshops/[a-z]+\.py$'),
     ('Linux', 'c', '\.c$') ON CONFLICT (repository_id) DO
 UPDATE
 SET filenames = EXCLUDED.filenames;
@@ -21,6 +23,7 @@ INSERT INTO git_guessr_game_format_config(
 VALUES ('GitOxide', '\.rs$', 30, false),
     ('Tree-sitter', '\.rs$', 30, false),
     ('GitGuessr', '\.rs$', 30, false),
+    ('Containerized Workshops', '\[^_].py$', 30, false),
     ('Linux', '\.c$', 30, false) ON CONFLICT (repository_id) DO
 UPDATE
 SET filenames = EXCLUDED.filenames,
